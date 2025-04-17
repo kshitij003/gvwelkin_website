eaa31601cd34adaf1acdcbef5eac2862f81f1391
kshitij003 <149072219+kshitij003@users.noreply.github.com>
2025-04-18 02:55:10 +0530

    Update page.jsx

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -17,7 +17,7 @@ export default function Page() {
         className="flex items-center justify-center px-8 py-8 sm:px-12 lg:col-span-7 lg:px-16 lg:py-12 xl:col-span-6"
         >
         <div className="max-w-xl lg:max-w-3xl">
-            <SignIn />
+            <SignIn redirectUrl="/" />
         </div>
         </main>
     </div>
